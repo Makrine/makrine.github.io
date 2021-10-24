e2c9c85f98adcb0057170d994449109c3e07bba3
Makrine <tmakrine@gmail.com>
2021-10-24 23:04:54 +0400

    added responisve

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import pic from './MakrineIco.png';
 import './App.css';
-import React from 'react';
+import React, { Component } from 'react';
 import { GitHub } from 'react-feather';
 import { Facebook } from 'react-feather';
 import { Instagram } from 'react-feather';
@@ -8,13 +8,26 @@ import { Mail } from 'react-feather';
 import Typist from "react-typist";
 
  
-function App() {
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { matches: window.matchMedia("(min-width: 768px)").matches };
+  }
+
+  componentDidMount() {
+    const handler = e => this.setState({matches: e.matches});
+    window.matchMedia("(min-width: 768px)").addEventListener('change', handler);
+  }
+  render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={pic} className="makrinePic" alt="MakrinePic" />
         <p id="homeText">
-          <Typist>
+          <Typist
+          cursor={{ show: true, hideWhenDone: true, hideWhenDoneDelay: 400 }}
+          >
           Hi there, I am Makrine!
           <Typist.Delay ms={700} />
           <br/><br/>
@@ -28,32 +41,45 @@ function App() {
             href="https://github.com/Makrine"
             target="_blank"
             rel="noopener noreferrer"
-          ><GitHub color="white" size={24} /></a>
+          >
+            {this.state.matches && (<GitHub color="white" size={24} />)}
+            {!this.state.matches && (<GitHub color="white" size={18} />)}
+          </a>
           <a
             id="Facebook"
             className="social-icons"
             href="https://facebook.com/m.tskipurishvili"
             target="_blank"
             rel="noopener noreferrer"
-          ><Facebook color="white" size={24} /></a>
+          >
+            {this.state.matches && (<Facebook color="white" size={24} />)}
+            {!this.state.matches && (<Facebook color="white" size={18} />)}
+          </a>
           <a
             id="Instagram"
             className="social-icons"
             href="https://www.instagram.com/makrine.tskipurishvili/"
             target="_blank"
             rel="noopener noreferrer"
-          ><Instagram color="white" size={24} /></a>
+          >
+            {this.state.matches && (<Instagram color="white" size={24} />)}
+            {!this.state.matches && (<Instagram color="white" size={18} />)}
+            </a>
           <a
             id="Gmail"
             className="social-icons"
             href="mailto:tskipurishvilimakrina@gmail.com?subject=Hey! Let's talk!"
             target="_blank"
             rel="noopener noreferrer"
-          ><Mail color="white" size={24} /></a> 
+          >
+            {this.state.matches && (<Mail color="white" size={24} />)}
+            {!this.state.matches && (<Mail color="white" size={18} />)}
+          </a> 
         </div>
 </header>
     </div>
   );
+}
 }
 
 export default App;
