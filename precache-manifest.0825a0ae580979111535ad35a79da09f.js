7bf73741087af3315de32fdef3d214343023a697
Makrine <tmakrine@gmail.com>
2021-10-24 21:05:35 +0400

    Updates

---
+++ precache-manifest.0825a0ae580979111535ad35a79da09f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8f0fa30e4382d92418ceb0c2e4d2f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c69ce956aed754bee9f7",
+    "url": "/static/css/main.4e34516e.chunk.css"
+  },
+  {
+    "revision": "f40fcdc2c893de733146",
+    "url": "/static/js/2.9269e028.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.9269e028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf40526b92db4acea1e5",
+    "url": "/static/js/3.79ed5269.chunk.js"
+  },
+  {
+    "revision": "c69ce956aed754bee9f7",
+    "url": "/static/js/main.345b7599.chunk.js"
+  },
+  {
+    "revision": "23eb85de31b2f9e3a88e",
+    "url": "/static/js/runtime-main.29d59e67.js"
+  },
+  {
+    "revision": "5dfd7badd78406b1dca0f22e8b129750",
+    "url": "/static/media/MakrineIco.5dfd7bad.png"
+  }
+]);
