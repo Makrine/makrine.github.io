3f0b2fb05dbe193d8345010747eec93f89f14445
Makrine <tmakrine@gmail.com>
2021-10-26 13:58:35 +0400

    added menu, scroll, contents

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import pic from './MakrineIco.png';
+import icon2D from './assets/MakrineIco.png';
+import realPic from './assets/makrine.jpg';
 import './App.css';
 import React, { Component } from 'react';
 import { GitHub } from 'react-feather';
@@ -6,34 +7,142 @@ import { Facebook } from 'react-feather';
 import { Instagram } from 'react-feather';
 import { Mail } from 'react-feather';
 import Typist from "react-typist";
+import ScrollToTop from "./ScrollToTop.js" 
+import { Link } from 'react-scroll';
 
- 
 class App extends Component {
+
 
   constructor(props) {
     super(props)
-    this.state = { matches: window.matchMedia("(min-width: 768px)").matches };
+    this.state = { 
+      matches: window.matchMedia("(min-width: 768px)").matches
+   };
   }
 
   componentDidMount() {
     const handler = e => this.setState({matches: e.matches});
     window.matchMedia("(min-width: 768px)").addEventListener('change', handler);
   }
+  
   render() {
+    
+    
   return (
-    <div className="App">
+    
+    <div className="App" id="top">
       <header className="App-header">
-        <img src={pic} className="makrinePic" alt="MakrinePic" />
+        <img 
+          src={this.picClicked?realPic:icon2D}
+          className="makrinePic"
+          alt="MakrinePic" 
+        />
         <p id="homeText">
           <Typist
           cursor={{ show: true, hideWhenDone: true, hideWhenDoneDelay: 400 }}
           >
           Hi there, I am Makrine!
-          <Typist.Delay ms={700} />
-          <br/><br/>
-          I can code.
           </Typist>
+          {/* <Typist
+          cursor={{ show: false}}
+          >
+          <Typist.Delay ms={2000} />
+          <br/>
+          I can code. 
+          </Typist> */}
         </p>
+        
+<div className="wrapper">
+  
+    <Link
+       to='whatIDo'
+       className='menu'
+       spy={true}
+       smooth={true}
+       offset={-70}
+       duration={500}>
+         <span>What I Do</span>
+       </Link>
+       
+       <Link
+       to='education'
+       className='menu'
+       spy={true}
+       smooth={true}
+       offset={-70}
+       duration={500}>
+         <span>Education</span>
+        </Link>
+
+      <Link
+       to='hobbies'
+       className='menu'
+       spy={true}
+       smooth={true}
+       offset={-70}
+       duration={500}>
+         <span>Hobbies</span>
+      </Link>
+      <Link
+       to='contact'
+       className='menu'
+       spy={true}
+       smooth={true}
+       offset={-70}
+       duration={500}>
+         <span>Contact</span>
+         </Link>
+</div>
+
+       
+
+
+</header>
+<body>
+
+       
+       
+<div class="content-container">
+  
+  <div class="box">
+    <div class="content" id="whatIDo">
+      <h2>What I do</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ipsa dolor veritatis quaerat. Amet cum est illo consequuntur exercitationem! A culpa placeat similique iure vero consectetur eos maxime necessitatibus sapiente!</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ipsa dolor veritatis quaerat. Amet cum est illo consequuntur exercitationem! A culpa placeat similique iure vero consectetur eos maxime necessitatibus sapiente!</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ipsa dolor veritatis quaerat. Amet cum est illo consequuntur exercitationem! A culpa placeat similique iure vero consectetur eos maxime necessitatibus sapiente!</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ipsa dolor veritatis quaerat. Amet cum est illo consequuntur exercitationem! A culpa placeat similique iure vero consectetur eos maxime necessitatibus sapiente!</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ipsa dolor veritatis quaerat. Amet cum est illo consequuntur exercitationem! A culpa placeat similique iure vero consectetur eos maxime necessitatibus sapiente!</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ipsa dolor veritatis quaerat. Amet cum est illo consequuntur exercitationem! A culpa placeat similique iure vero consectetur eos maxime necessitatibus sapiente!</p>
+    </div>
+  </div>
+  
+    <div class="box">
+      <div class="content" id="education">
+    <h2>Education</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ipsa dolor veritatis quaerat. Amet cum est illo consequuntur exercitationem! A culpa placeat similique iure vero consectetur eos maxime necessitatibus sapiente!</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ipsa dolor veritatis quaerat. Amet cum est illo consequuntur exercitationem! A culpa placeat similique iure vero consectetur eos maxime necessitatibus sapiente!</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ipsa dolor veritatis quaerat. Amet cum est illo consequuntur exercitationem! A culpa placeat similique iure vero consectetur eos maxime necessitatibus sapiente!</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ipsa dolor veritatis quaerat. Amet cum est illo consequuntur exercitationem! A culpa placeat similique iure vero consectetur eos maxime necessitatibus sapiente!</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ipsa dolor veritatis quaerat. Amet cum est illo consequuntur exercitationem! A culpa placeat similique iure vero consectetur eos maxime necessitatibus sapiente!</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ipsa dolor veritatis quaerat. Amet cum est illo consequuntur exercitationem! A culpa placeat similique iure vero consectetur eos maxime necessitatibus sapiente!</p>
+      </div>
+  </div>
+  
+    <div class="box">
+      <div class="content" id="hobbies">
+        <h2>Hobbies</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ipsa dolor veritatis quaerat. Amet cum est illo consequuntur exercitationem! A culpa placeat similique iure vero consectetur eos maxime necessitatibus sapiente!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ipsa dolor veritatis quaerat. Amet cum est illo consequuntur exercitationem! A culpa placeat similique iure vero consectetur eos maxime necessitatibus sapiente!</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ipsa dolor veritatis quaerat. Amet cum est illo consequuntur exercitationem! A culpa placeat similique iure vero consectetur eos maxime necessitatibus sapiente!</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ipsa dolor veritatis quaerat. Amet cum est illo consequuntur exercitationem! A culpa placeat similique iure vero consectetur eos maxime necessitatibus sapiente!</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ipsa dolor veritatis quaerat. Amet cum est illo consequuntur exercitationem! A culpa placeat similique iure vero consectetur eos maxime necessitatibus sapiente!</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ipsa dolor veritatis quaerat. Amet cum est illo consequuntur exercitationem! A culpa placeat similique iure vero consectetur eos maxime necessitatibus sapiente!</p>
+      </div>
+  </div>
+
+  <div class="box">
+      <div class="content" id="contact">
+        <h2>Contact</h2>
         <div className="socialIcons">
           <a
             id="GitHub"
@@ -76,7 +185,17 @@ class App extends Component {
             {!this.state.matches && (<Mail color="white" size={18} />)}
           </a> 
         </div>
-</header>
+      </div>
+  </div>
+  
+      </div>
+
+
+
+</body>
+
+<ScrollToTop/>
+        
     </div>
   );
 }
---
+++ src/ScrollToTop.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import top from "./assets/arrowUp.png"
+export default function ScrollToTop() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+  }, []);
+
+  return (
+    <div className="scroll-to-top">
+      {isVisible && 
+        <div onClick={scrollToTop}>
+          <img src={top} alt='Go to top'/>
+        </div>}
+    </div>
+  );
+}
