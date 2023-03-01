81b3c4dec50319a7b23d9298424059d36b5b8f60
Makrine <tmakrine@gmail.com>
2023-03-01 22:52:08 +0400

    new build

---
+++ build/precache-manifest.6beed8c74979ca5cc2aa00d477c9fc7e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8917b8b462700b95902ca87093db4d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "444fced3d294945c726f",
+    "url": "/static/css/main.2b04fd90.chunk.css"
+  },
+  {
+    "revision": "dd70365b2f1dd4f6e4f2",
+    "url": "/static/js/2.9dd7ae24.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.9dd7ae24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51402aa77fbcb3f8510e",
+    "url": "/static/js/3.ca2f7720.chunk.js"
+  },
+  {
+    "revision": "444fced3d294945c726f",
+    "url": "/static/js/main.2db00129.chunk.js"
+  },
+  {
+    "revision": "78969fabac9fa9b2660c",
+    "url": "/static/js/runtime-main.47c7c307.js"
+  },
+  {
+    "revision": "5dfd7badd78406b1dca0f22e8b129750",
+    "url": "/static/media/MakrineIco.5dfd7bad.png"
+  },
+  {
+    "revision": "35a0846a1034b61bfab21364740e05d2",
+    "url": "/static/media/makrine.35a0846a.jpg"
+  }
+]);
